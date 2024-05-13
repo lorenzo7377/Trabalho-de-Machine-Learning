1797f4216c75d02fa45d272481f0823d05c0fa09
pkbceira03 <cabeceira2003@gmail.com>
2024-05-13 18:03:15 -0300

    entrega 6

---
+++ entregáveis/entrega 6/predicao.py
@@ -0,0 +1,76 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
+from sklearn.metrics import classification_report, confusion_matrix
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('./databasejogos.csv')
+
+features = [
+    'FG_PCT_home', 'FT_PCT_home', 'FG3_PCT_home', 'AST_home', 'REB_home',
+    'FG_PCT_away', 'FT_PCT_away', 'FG3_PCT_away', 'AST_away', 'REB_away'
+]
+target = 'HOME_TEAM_WINS'
+
+X = df[features].fillna(0)  # Tratamento de possíveis valores faltantes
+y = df[target]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+logistic_model = LogisticRegression()
+logistic_model.fit(X_train, y_train)
+y_pred = logistic_model.predict(X_test)
+
+print('porcentagem ', metrics.accuracy_score(y_test, y_pred))
+
+predictions = logistic_model.predict(X_test)
+print("Classification Report:")
+print(classification_report(y_test, predictions))
+print("Confusion Matrix:")
+print(confusion_matrix(y_test, predictions))
+
+time_desejado = 1610612751
+
+# Filtragem dos dados para o time desejado
+df_time = df[(df['HOME_TEAM_ID'] == time_desejado) | (df['VISITOR_TEAM_ID'] == time_desejado)].copy()
+
+# Tratamento de valores ausentes
+df_time.loc[df_time['HOME_TEAM_ID'] == time_desejado, 'PTS_home'].fillna(0, inplace=True)
+df_time.loc[df_time['HOME_TEAM_ID'] == time_desejado, 'FG_PCT_home'].fillna(0, inplace=True)
+df_time.loc[df_time['HOME_TEAM_ID'] == time_desejado, 'FT_PCT_home'].fillna(0, inplace=True)
+df_time.loc[df_time['HOME_TEAM_ID'] == time_desejado, 'FG3_PCT_home'].fillna(0, inplace=True)
+df_time.loc[df_time['HOME_TEAM_ID'] == time_desejado, 'AST_home'].fillna(0, inplace=True)
+df_time.loc[df_time['HOME_TEAM_ID'] == time_desejado, 'REB_home'].fillna(0, inplace=True)
+
+media_pontos_casa = df_time[df_time['HOME_TEAM_ID'] == time_desejado]['PTS_home'].mean()
+media_fgpct_casa = df_time[df_time['HOME_TEAM_ID'] == time_desejado]['FG_PCT_home'].mean()
+media_ftpct_casa = df_time[df_time['HOME_TEAM_ID'] == time_desejado]['FT_PCT_home'].mean()
+media_fg3_casa = df_time[df_time['HOME_TEAM_ID'] == time_desejado]['FG3_PCT_home'].mean()
+media_ast_casa = df_time[df_time['HOME_TEAM_ID'] == time_desejado]['AST_home'].mean()
+media_reb_casa = df_time[df_time['HOME_TEAM_ID'] == time_desejado]['REB_home'].mean()
+print(media_pontos_casa)
+print(media_fgpct_casa)
+print(media_ftpct_casa)
+print(media_fg3_casa)
+print(media_ast_casa)
+print(media_reb_casa)
+
+teste_vencedor = { 
+    'FG_PCT_home': 0.5, 'FT_PCT_home':0.54, 'FG3_PCT_home':0.39, 'AST_home':27, 'REB_home':48,
+    'FG_PCT_away':0.6, 'FT_PCT_away':0.7, 'FG3_PCT_away':0.6, 'AST_away':34, 'REB_away':57
+}
+
+dft = pd.DataFrame(data = teste_vencedor, index=[0])
+
+print(dft)
+
+resultado = logistic_model.predict(dft)
+resultado2 = logistic_model.predict_proba(dft)
+
+if (resultado == 0):
+    print("Time da visitante venceu")
+else:
+    print("Time da casa venceu")
+
+print(resultado2)
